16836234d59e23938258aaa9a1cacd76902b0e68
Abhishek Khangare <abhishekkhangare21@gmail.com>
2024-02-20 09:10:49 +0530

    update product card image source and add a product card component to the home page

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
+import ProductCard from "../components/product-card";
 
 const Home = () => {
+  const addToCartHandler = () => {};
   return (
     <div className="home">
       <section></section>
@@ -12,7 +14,16 @@ const Home = () => {
         </Link>
       </h1>
 
-      <main></main>
+      <main>
+        <ProductCard
+          productId="adsaskd"
+          name="Macbook"
+          price={349}
+          stock={43}
+          handler={addToCartHandler}
+          photo="https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/macbook-air-space-gray-select-201810?wid=904&hei=840&fmt=jpeg&qlt=90&.v=1664472289661"
+        />
+      </main>
     </div>
   );
 };
