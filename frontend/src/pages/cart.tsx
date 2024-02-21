3093a52c2114231860bf5ba9ec418b3b126f527a
Abhishek Khangare <abhishekkhangare21@gmail.com>
2024-02-21 11:38:26 +0530

    add coupon code feature to the cart page

--- frontend/src/pages/cart.tsx
+++ frontend/src/pages/cart.tsx
@@ -1,5 +1,51 @@
+import { useState } from "react";
+import { VscError } from "react-icons/vsc";
+
+const cartItems = [];
+const subtotal = 4000;
+const tax = Math.round(subtotal * 0.18);
+const shippingCharges = 200;
+const discount = 400;
+const total = subtotal + tax + shippingCharges;
+
 function Cart() {
-  return <div>Cart</div>;
+  const [coupenCode, setCoupenCode] = useState<string>("");
+  const [isValidCoupenCode, setIsValidCoupenCode] = useState<boolean>(false);
+
+  return (
+    <div className="cart">
+      <main></main>
+      <aside>
+        <p>Subtotal: ₹{subtotal}</p>
+        <p>Shipping Charges: ₹{shippingCharges}</p>
+        <p>Tax: ₹{tax}</p>
+        <p>
+          Discount: <em> - ₹{discount}</em>
+        </p>
+        <p>
+          <b>Total: ₹{total}</b>
+        </p>
+
+        <input
+          type="text"
+          placeholder="Coupen Code"
+          value={coupenCode}
+          onChange={(e) => setCoupenCode(e.target.value)}
+        />
+
+        {coupenCode &&
+          (isValidCoupenCode ? (
+            <span className="green">
+              ₹{discount} off using the <code>{coupenCode}</code>
+            </span>
+          ) : (
+            <span className="red">
+              Invalid Coupen Code <VscError />
+            </span>
+          ))}
+      </aside>
+    </div>
+  );
 }
 
 export default Cart;
