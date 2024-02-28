a863c7d590df67099e17ad28fc254b6260fda92b
Abhishek Khangare <abhishekkhangare21@gmail.com>
2024-02-28 09:09:55 +0530

    Update frontend code for cart page and styles

--- frontend/src/pages/cart.tsx
+++ frontend/src/pages/cart.tsx
@@ -1,7 +1,20 @@
 import { useEffect, useState } from "react";
 import { VscError } from "react-icons/vsc";
+import CartItem from "../components/cart-item";
+import { Link } from "react-router-dom";
 
-const cartItems = [];
+const cartItems = [
+  {
+    productId: "aasdfasd",
+    photo:
+      "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/macbook-air-space-gray-select-201810?wid=904&hei=840&fmt=jpeg&qlt=90&.v=1664472289661",
+    name: "Mackbook",
+    price: 3000,
+    quantity: 4,
+    stock: 10,
+  },
+];
+
 const subtotal = 4000;
 const tax = Math.round(subtotal * 0.18);
 const shippingCharges = 200;
@@ -29,13 +42,19 @@ function Cart() {
 
   return (
     <div className="cart">
-      <main></main>
+      <main>
+        {cartItems.length > 0 ? (
+          cartItems.map((item, idx) => <CartItem key={idx} cartItem={item} />)
+        ) : (
+          <h1>No Items Added</h1>
+        )}
+      </main>
       <aside>
         <p>Subtotal: ₹{subtotal}</p>
         <p>Shipping Charges: ₹{shippingCharges}</p>
         <p>Tax: ₹{tax}</p>
         <p>
-          Discount: <em> - ₹{discount}</em>
+          Discount: <em className="red"> - ₹{discount}</em>
         </p>
         <p>
           <b>Total: ₹{total}</b>
@@ -58,6 +77,8 @@ function Cart() {
               Invalid Coupen Code <VscError />
             </span>
           ))}
+
+        {cartItems.length > 0 && <Link to="/shipping">Checkout</Link>}
       </aside>
     </div>
   );
