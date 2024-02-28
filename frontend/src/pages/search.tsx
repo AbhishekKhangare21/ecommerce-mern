baa219d213c443b1c1356528e984d48c04acdbe5
Abhishek Khangare <abhishekkhangare21@gmail.com>
2024-02-28 11:15:11 +0530

    Update search page

--- frontend/src/pages/search.tsx
+++ frontend/src/pages/search.tsx
@@ -1,5 +1,94 @@
+import { useState } from "react";
+import ProductCard from "../components/product-card";
+
 const Search = () => {
-  return <div>Search</div>;
+  const [search, setSearch] = useState("");
+  const [sort, setSort] = useState("");
+  const [maxPrice, setMaxPrice] = useState(100000);
+  const [category, setCategory] = useState("");
+  const [page, setPage] = useState(1);
+
+  const addToCartHandler = () => {};
+
+  const isPrevPage = true;
+  const isNextPage = true;
+
+  return (
+    <div className="product-search-page">
+      <aside>
+        <h2>Filters</h2>
+
+        <div>
+          <h4>Sort</h4>
+          <select value={sort} onChange={(e) => setSort(e.target.value)}>
+            <option value="">None</option>
+            <option value="asc">Price (Low to High)</option>
+            <option value="dsc">Pirce (High to Low)</option>
+          </select>
+        </div>
+
+        <div>
+          <h4>Max Price:{maxPrice || ""}</h4>
+          <input
+            type="range"
+            min={100}
+            max={100000}
+            value={maxPrice}
+            onChange={(e) => setMaxPrice(Number(e.target.value))}
+          />
+        </div>
+
+        <div>
+          <h4>Category</h4>
+          <select
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+          >
+            <option value="">All</option>
+            <option value="">Sample 1</option>
+            <option value="">Sample 2</option>
+          </select>
+        </div>
+      </aside>
+      <main>
+        <h1>Products</h1>
+        <input
+          type="text"
+          placeholder="Search by name..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+
+        <div className="search-product-list">
+          <ProductCard
+            productId="adsaskd"
+            name="Macbook"
+            price={349}
+            handler={addToCartHandler}
+            photo="https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/macbook-air-space-gray-select-201810?wid=904&hei=840&fmt=jpeg&qlt=90&.v=1664472289661"
+          />
+        </div>
+
+        <article>
+          <button
+            disabled={!isPrevPage}
+            onClick={() => setPage((prev) => prev - 1)}
+          >
+            Prev
+          </button>
+          <span>
+            {page} of {4}
+          </span>
+        </article>
+        <button
+          disabled={!isNextPage}
+          onClick={() => setPage((prev) => prev + 1)}
+        >
+          Next
+        </button>
+      </main>
+    </div>
+  );
 };
 
 export default Search;
