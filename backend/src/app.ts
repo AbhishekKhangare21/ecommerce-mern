54b80f91e944c0f13b07da19bd66a6d7f0c0155c
Abhishek Khangare <abhishekkhangare21@gmail.com>
2024-02-29 21:33:25 +0530

    feat: implement node-cache for product and order routes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,10 +2,13 @@ import express from "express";
 // Importing Routes
 import userRoute from "./routes/user.js";
 import { connectDB } from "./utils/features.js";
+import NodeCache from "node-cache";
 
 const port = 4000;
 
 connectDB();
+
+export const myCache = new NodeCache();
 
 const app = express();
 
