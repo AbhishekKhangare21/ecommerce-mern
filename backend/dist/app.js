52dcc5153f9de9948db2b625f46307ef45206910
Abhishek Khangare <abhishekkhangare21@gmail.com>
2024-03-01 13:40:11 +0530

    Add order route and controller

--- backend/dist/app.js
+++ backend/dist/app.js
@@ -2,6 +2,7 @@ import express from "express";
 // Importing Routes
 import userRoute from "./routes/user.js";
 import productRoute from "./routes/product.js";
+import orderRoute from "./routes/order.js";
 import { connectDB } from "./utils/features.js";
 import NodeCache from "node-cache";
 const port = 4000;
@@ -12,6 +13,7 @@ app.use(express.json());
 // Using Routes
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/product", productRoute);
+app.use("/api/v1/order", orderRoute);
 app.listen(port, () => {
     console.log(`Express is working on http://localhost:${port}`);
 });
--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,6 +3,7 @@ import express from "express";
 // Importing Routes
 import userRoute from "./routes/user.js";
 import productRoute from "./routes/product.js";
+import orderRoute from "./routes/order.js";
 import { connectDB } from "./utils/features.js";
 import NodeCache from "node-cache";
 
@@ -19,6 +20,7 @@ app.use(express.json());
 // Using Routes
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/product", productRoute);
+app.use("/api/v1/order", orderRoute);
 
 app.listen(port, () => {
   console.log(`Express is working on http://localhost:${port}`);
