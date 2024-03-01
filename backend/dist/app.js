68a8c0f8aee4cf8b680548d1ab2ab7c7ef8425e5
Abhishek Khangare <abhishekkhangare21@gmail.com>
2024-03-01 07:24:40 +0530

    Add caching and data manipulation functions

--- backend/dist/app.js
+++ backend/dist/app.js
@@ -1,13 +1,17 @@
 import express from "express";
 // Importing Routes
 import userRoute from "./routes/user.js";
+import productRoute from "./routes/product.js";
 import { connectDB } from "./utils/features.js";
+import NodeCache from "node-cache";
 const port = 4000;
 connectDB();
+export const myCache = new NodeCache();
 const app = express();
 app.use(express.json());
 // Using Routes
 app.use("/api/v1/user", userRoute);
+app.use("/api/v1/product", productRoute);
 app.listen(port, () => {
     console.log(`Express is working on http://localhost:${port}`);
 });
