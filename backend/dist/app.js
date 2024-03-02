fe91b940f9e9f9ee36b17e7560d94ff72063f64d
Abhishek Khangare <abhishekkhangare21@gmail.com>
2024-03-02 16:55:00 +0530

    Added routes for payment and dashboard

--- backend/dist/app.js
+++ backend/dist/app.js
@@ -10,6 +10,8 @@ import cors from "cors";
 import userRoute from "./routes/user.js";
 import productRoute from "./routes/product.js";
 import orderRoute from "./routes/order.js";
+import paymentRoute from "./routes/payment.js";
+import dashboardRoute from "./routes/stats.js";
 config({
     path: "./.env",
 });
@@ -23,10 +25,15 @@ const app = express();
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(cors());
+app.get("/", (req, res) => {
+    res.send("API Working with /api/v1");
+});
 // Using Routes
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/product", productRoute);
 app.use("/api/v1/order", orderRoute);
+app.use("/api/v1/payment", paymentRoute);
+app.use("/api/v1/dashboard", dashboardRoute);
 app.use("/uploads", express.static("uploads"));
 app.use(errorMiddleware);
 app.listen(port, () => {
