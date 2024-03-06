08c639bbc597d2926711a17d738516dd59d79703
Abhishek Khangare <abhishekkhangare21@gmail.com>
2024-03-06 10:46:12 +0530

    Update product images with local images from db/images.ts

--- frontend/db/images.ts
+++ frontend/db/images.ts
@@ -1,6 +1,8 @@
 export const imgData: { id: number; images: string[] }[] = [
   {
     id: 1,
-    images: ["https://www.scaler.com/topics/images/mrinal_bhattacharya.webp"],
+    images: [
+      "https://rukminim2.flixcart.com/image/416/416/xif0q/mobile/l/x/c/-original-imagx6rdpmhuq5ba.jpeg?q=70&crop=false",
+    ],
   },
 ];
