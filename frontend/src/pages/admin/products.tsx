9ae7ee28e90bb43be268ba2066b38a7c61447b4c
Abhishek Khangare <abhishekkhangare21@gmail.com>
2024-03-06 14:32:56 +0530

    Remove unnecessary import of `server` from `redux/store`.=

--- frontend/src/pages/admin/products.tsx
+++ frontend/src/pages/admin/products.tsx
@@ -8,7 +8,8 @@ import AdminSidebar from "../../components/admin/AdminSidebar";
 import TableHOC from "../../components/admin/TableHOC";
 import { Skeleton } from "../../components/loader";
 import { useAllProductsQuery } from "../../redux/api/productAPI";
-import { RootState, server } from "../../redux/store";
+// import { RootState, server } from "../../redux/store";
+import { RootState } from "../../redux/store";
 import { CustomError } from "../../types/api-types";
 import { imgData } from "../../../db/images";
 interface DataType {
